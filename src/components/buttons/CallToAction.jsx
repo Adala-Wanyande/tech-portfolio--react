61d45cb4e38d9474da817463f5103b7469a5c0b1
Adala Wanyande <awanyande@gmail.com>
2022-06-25 14:45:17 +0300

    Download CV feature implemented

--- src/components/buttons/CallToAction.jsx
+++ src/components/buttons/CallToAction.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import CV from '../../assets/CV - Adala Wanyande.pdf'
 
 function CallToAction() {
   return (
     <div className='font-bold rounded flex text-sm justify-center content-center drop-shadow-md text-center bg-gray-800 text-white px-4 py-2 mt-8 hover:cursor-pointer hover:bg-gray-600'>
-      <a className=''>Download CV</a>
+      <a href={CV} download>Download CV</a>
     </div>
   )
 } 
