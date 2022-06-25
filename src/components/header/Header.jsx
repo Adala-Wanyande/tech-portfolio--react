762a08c719c6dc270e0a1c617e82b32055563402
Adala Wanyande <awanyande@gmail.com>
2022-06-25 13:26:55 +0300

    Double chevron added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -30,8 +30,11 @@ const Header = () => {
           </a>
         </div>
       </div>
-      <div className='flex justify-center hover:cursor-pointer'>
-        <i class="fa-solid fa-chevron-down"></i>
+      <div className='flex justify-center hover:cursor-pointer mt-4 h-8'>
+        <svg className='hover:w-8 hover:h-8 text-gray-400' width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M17 13L12 17L7 13" stroke="#4A4B57" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M17 7L12 11L7 7" stroke="#4A4B57" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
       </div>
       
     </div>  
